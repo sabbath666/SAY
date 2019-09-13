0b999872cd4667bd0aa6a000ce6a512a3803cbbc
sabbath666 <sergeysabbath@gmail.com>
2019-09-13 11:06:45 +0300

    refactoring

--- say/src/main/kotlin/com/sabbath/say/sayservice/client/QuoteClient.kt
+++ say/src/main/kotlin/com/sabbath/say/sayservice/client/QuoteClient.kt
@@ -2,6 +2,7 @@ package com.sabbath.say.sayservice.client
 
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+import java.util.*
 
 @Component
 class QuoteClient(
@@ -9,10 +10,14 @@ class QuoteClient(
 ) {
 
     fun getQuote(): String? {
-        val result = quoteRestTemplate.getForObject(
-                "/quote",
-                String::class.java
-        )
+        val result = try {
+            quoteRestTemplate.getForObject(
+                    "/quote",
+                    String::class.java
+            )
+        } catch (e: Exception) {
+            Base64.getEncoder().encodeToString(e.message!!.substring(0, 100).plus("...ЭТО ПИПЕЦ!!").toByteArray())
+        }
         return result;
     }
 }
