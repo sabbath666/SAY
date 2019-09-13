a6bc47e5a04adba4f5738d83845b4fc1a2962d00
sabbath666 <sergeysabbath@gmail.com>
2019-09-13 12:39:42 +0300

    refactoring

--- quote/src/main/kotlin/com/sabbath/say/quoteservice/QuoteServiceDev.kt
+++ quote/src/main/kotlin/com/sabbath/say/quoteservice/QuoteServiceDev.kt
@@ -1,13 +1,13 @@
 //package com.sabbath.say.quoteservice
 //
 //import com.github.javafaker.Faker
+//import org.springframework.context.annotation.Profile
 //import org.springframework.stereotype.Service
 //
 //@Service
-//class QuoteServiceImpl(
+//class QuoteServiceDev(
 //        private val faker: Faker = Faker()
 //) : QuoteService {
 //    override fun quote(): String =
 //            faker.lebowski().quote()
-//
 //}
--- quote/src/main/kotlin/com/sabbath/say/quoteservice/QuoteServiceGreenElephant.kt
+++ quote/src/main/kotlin/com/sabbath/say/quoteservice/QuoteServiceGreenElephant.kt
@@ -1,6 +1,7 @@
 package com.sabbath.say.quoteservice
 
 import com.github.javafaker.Faker
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 import java.util.*
 
