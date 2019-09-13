4b99bb1ba3e6c9ed38752b84be1b244fb3bb2e49
sabbath666 <sergeysabbath@gmail.com>
2019-09-13 06:18:23 +0300

    refactoring

--- say/src/main/kotlin/com/sabbath/say/sayservice/SayController.kt
+++ say/src/main/kotlin/com/sabbath/say/sayservice/SayController.kt
@@ -2,6 +2,7 @@ package com.sabbath.say.sayservice
 
 import com.sabbath.say.sayservice.client.MonsterClient
 import com.sabbath.say.sayservice.client.QuoteClient
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,10 +11,11 @@ class SayController(
         val monsterClient: MonsterClient,
         val quoteClient: QuoteClient
 ) {
-    @GetMapping
+    @GetMapping("/")
     fun say(): String {
         val message = quoteClient.getQuote()!!
         val result=monsterClient.say(message)!!
         return result
     }
+
 }
