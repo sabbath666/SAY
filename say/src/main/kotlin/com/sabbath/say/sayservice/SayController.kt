94d4b5039d8ed0bb8c53b3f9fbc7e28a38a8c237
sabbath666 <sergeysabbath@gmail.com>
2019-09-13 06:37:07 +0300

    refactoring

--- say/src/main/kotlin/com/sabbath/say/sayservice/SayController.kt
+++ say/src/main/kotlin/com/sabbath/say/sayservice/SayController.kt
@@ -11,20 +11,22 @@ class SayController(
         val monsterClient: MonsterClient,
         val quoteClient: QuoteClient
 ) {
-    @GetMapping
+    @GetMapping("/say")
     fun say(): String {
         println("===> say")
-        var result=""
+        var result = ""
         try {
             val message = quoteClient.getQuote()!!
             println("message")
             result = monsterClient.say(message)!!
             println(result)
+        } catch (ex: Exception) {
+            println(ex)
         }
-        catch (ex:Exception)
-        {
-            println(ex)}
         return result
     }
+
+    @GetMapping("/test")
+    fun test()="test"
 
 }
